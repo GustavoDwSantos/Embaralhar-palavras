8b578ca27e1b2c33d2a60964866e2d7dc3ee6c2c
GustavoDwSantos <gustavos200116@gmail.com>
2022-08-29 10:42:54 -0300

    Alteração do programa

--- embaralhador_de_palavras.py
+++ embaralhador_de_palavras.py
@@ -6,16 +6,29 @@ def sorteadordepalavra(lista):
     localizacao = random.randrange(0, len(lista))
     return lista[localizacao]
 
-
 def embaralhadordestring(s):
     ns = list(s)
     random.shuffle(ns)
     return "".join(ns)
 
+def testador_tentativas(numero_tentativas, palavra_aleatoria, lista_com_frases_motivacionais): 
+    tentativas = 1
+    while tentativas < numero_tentativas + 1:
+        chute = input("qual a palavra embaralhada?")
+        if chute == palavra_aleatoria:
+            print(f"Parabens, você acertou!!! \nTotal de tentativas:{tentativas}")
+            break
+        elif tentativas == numero_tentativas:
+            break
+        else:
+            print (sorteadordepalavra(lista_com_frases_motivacionais))
+        tentativas += 1 
+        if numero_tentativas == tentativas:
+            print (f"Que pena, acabou a quantidade de tentativas!\nA palavra correta era {palavra_aleatoria}")
 
 def main():
 
-    tentativas = 0
+    
     lista_com_palavras = [
         "abacate",
         "saci",
@@ -34,21 +47,15 @@ def main():
         "jovem",
     ]
     lista_com_frases_motivacionais = ["Tente novamente", "Não foi dessa vez, não desista", "O universo é imenso, você é só um grão de areia, não desista", "Na proxima tentativa você consegue!", "foi quase"]
+    numero_tentativas = 5
     palavra_aleatoria = sorteadordepalavra(lista_com_palavras)
     palavra_embaralhada = embaralhadordestring(palavra_aleatoria)
-
+    
     print(
         f"Jogo de palavra embaralhada \nA palavra sorteada foi { palavra_embaralhada }"
     )
 
-    while tentativas < 5:
-        chute = input("qual a palavra embaralhada?")
-        if chute == palavra_aleatoria:
-            print("Parabens, você acertou!!!")
-            break
-        else:
-            print (sorteadordepalavra(lista_com_frases_motivacionais))
-        tentativas += 1 
+    testador_tentativas(numero_tentativas, palavra_aleatoria, lista_com_frases_motivacionais)
 
 if __name__ == "__main__":
     main()
