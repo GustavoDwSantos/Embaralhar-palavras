98e5833465accbd674140cb0c69d18d9bf0a0241
GustavoDwSantos <gustavos200116@gmai.com>
2022-09-14 18:45:02 -0300

    adição de dificuldade e escolha de tema

---
+++ dificuldade.py
@@ -0,0 +1,17 @@
+def escolha_dificuldade():
+    print ("escolha a quantidade de letras da palavra")
+    while True:
+        letra = input("1 - Facil (Menos que 5 letras)\n2 - Medio (entre 6 e 7 letras)\n3 - Dificil (Mais que 8 Letras)")
+        letras = {"1":"5","2":"7","3":"8"}
+        if letra in letras:
+            return letras[letra]
+        else:
+            print("Valor invalido - Tente novamente")
+            continue
+
+def limitador_caracter (lista,dificuldade):
+    if dificuldade == "5":
+        return [item for item in lista if len(item) <= 5]
+    elif dificuldade == "7":
+        return [item for item in lista if len(item) in (6,7)]
+    else: return [item for item in lista if len(item) >= 8]
---
+++ escolha_tema.py
@@ -0,0 +1,12 @@
+def escolha_tema():
+    print ("escolha um tema:")
+    while True:
+        tema = input ("1 - Frutas\n2 - Cores\n3 - Comidas\n4 - Palavras Aleatorias\nDigite um numero para selecionar um tema\n")
+        temas = {"1":"Frutas.txt","2":"Cores.txt","3":"Comidas.txt","4":"Aleatorio.txt"}
+        if tema in temas:
+            with open (f'./palavras/{temas[tema]}') as file:
+                texto = file.read().lower()
+            return texto.split()
+        else:
+            print("Valor invalido - Tente novamente")
+            continue        
